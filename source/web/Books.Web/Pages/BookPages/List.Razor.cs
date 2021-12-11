babe7f05748bbea552b3f3da6c76e89fee8c79a3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-12-11 13:28:46 +0530

    After deploying the UI

---
+++ source/web/Books.Web/Pages/BookPages/List.Razor.cs
@@ -0,0 +1,24 @@
+﻿using Books.Data;
+using Books.DataServices;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Books.Web.Pages.BookPages
+{
+
+    public partial class List
+    {
+        [Inject]
+        private IBookDataService BookDataService { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = await BookDataService.GetAllBooks();
+        }
+
+    }
+
+}
--- source/web/Books.Web/Program.cs
+++ source/web/Books.Web/Program.cs
@@ -1,3 +1,4 @@
+using Books.DataServices;
 using Books.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IBookDataService, BookDataService>(client =>
+    {
+        client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]);
+    }
+);
 
 await builder.Build().RunAsync();
