c98ec2c01defe634061fc4f9685c1669cabc77db
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-02-12 12:37:15 +0530

    ..

--- source/web/Books.Web/Program.cs
+++ source/web/Books.Web/Program.cs
@@ -11,7 +11,10 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddHttpClient<IBookDataService, BookDataService>(client =>
     {
-        client.BaseAddress = new Uri(builder.Configuration["WebApis:Books"]);
+#pragma warning disable CS8604 // Possible null reference argument.
+        client.BaseAddress = new Uri(builder?.Configuration["WebApis:Books"]);
+#pragma warning restore CS8604 // Possible null reference argument.
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", builder.Configuration["WebApis:ApimSubscriptionKey"]);
     }
 );
 
