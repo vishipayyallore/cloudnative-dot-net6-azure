d97eab821a101b8c2ee80ed48da4f42f20de9784
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-04-05 11:44:57 +0530

    Initial Version

---
+++ source/web/Books.WebApp/Pages/BookPages/List.Razor.cs
@@ -0,0 +1,22 @@
+﻿using Books.Data;
+using Books.DataServices;
+using Microsoft.AspNetCore.Components;
+
+namespace Books.WebApp.Pages.BookPages
+{
+
+    public partial class List
+    {
+        [Inject]
+        private IBookDataService BookDataService { get; set; }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = await BookDataService.GetAllBooks();
+        }
+
+    }
+
+}
